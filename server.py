405c42c7bf0e93cf0b843f23b86fc6b0a14a4948
wangxu724 <wangxu724@gmail.com>
2015-11-19 01:25:16 -0700

    Update server.py

--- server.py
+++ server.py
@@ -2,10 +2,16 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SimpleHTTPServer
 import SocketServer
 import sys
+import dataManager
+import json
 
 
 
 class MyHttpRequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, request, client_address, server):
+        self.DM = dataManager.DataManager('db',10)
+        BaseHTTPRequestHandler.__init__(self, request, client_address, server)
+
     def do_GET(self):
         rootdir = './pages/' #file location
         try:
@@ -22,17 +28,22 @@ class MyHttpRequestHandler(BaseHTTPRequestHandler):
                 #send file content to client
                 self.wfile.write(f.read())
                 f.close()
-                return
-          
         except IOError:
           self.send_error(404, 'file not found')
+        return
 
 
     def do_POST(self):
-        if self.headers['Content-type'] == "application/json":
-            length = int(self.headers['Content-Length'])
-            self.send_response(200)
-            print self.rfile.read(length)
+        try:
+            if self.headers['Content-type'] == "application/json":
+                length = int(self.headers['Content-Length'])
+                self.send_response(200)
+                data  = json.loads(self.rfile.read(length))
+                username = data['username']
+                encryptedPattern = data['cyphertext']
+
+        except IOError as e:
+            self.send_error(str(e))
         pass
 
 
@@ -45,7 +56,6 @@ if __name__ == '__main__':
 
     print('http server is starting...')
     #ip and port of servr
-    #by default http server port is 80
     server_address = (sys.argv[1], int(sys.argv[2]))
     httpd = HTTPServer(server_address, MyHttpRequestHandler)
     print('http server is running...')
