80e430029669050c427858ab5fdbd45770bc4060
swift-fox <kyle@swift-fox.com>
2015-11-19 15:21:54 -0700

    Define the interface for server-side data decryption

---
+++ decrypt.py
@@ -0,0 +1,15 @@
+def decrypt(ciphertext, password):
+    # Generate key from password
+
+    # Decrypt the ciphertext
+    
+    # Extract the pattern and compute the hash
+
+    # Extract the hash and compare
+
+    # If match
+        # Convert the pattern into Python array
+        # return the array
+    # else
+        # return false
+    pass
